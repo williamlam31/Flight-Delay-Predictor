5e5b1161ee5f363fecd0a06d79fa4385e2e9f333
williamlam31 <williamlam31@gmail.com>
2025-08-11 17:22:17 -0400

    Update flight_app.py

--- flight_app.py
+++ flight_app.py
@@ -118,13 +118,11 @@ with col3:
 
 if st.button("Predict Flight Status with ALL Models", type="primary", use_container_width=True):
     try:
-
-def time_to_minutes(t):
-    t = str(int(t)).zfill(4)
-    return int(t[:2]) * 60 + int(t[2:])
-
-dep_minutes = time_to_minutes(crs_dep_time)
-arr_minutes = time_to_minutes(crs_arr_time)
+        def time_to_minutes(t):
+            t = str(int(t)).zfill(4)
+            return int(t[:2]) * 60 + int(t[2:])
+        dep_minutes = time_to_minutes(crs_dep_time)
+        arr_minutes = time_to_minutes(crs_arr_time)
 
 crs_elapsed_time = (arr_minutes - dep_minutes) % (24 * 60)
      
