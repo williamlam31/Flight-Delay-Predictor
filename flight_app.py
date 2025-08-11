9a9dd85df936706a13277b0a3ba291864c5d8be2
williamlam31 <williamlam31@gmail.com>
2025-08-11 18:30:06 -0400

    Update flight_app.py

--- flight_app.py
+++ flight_app.py
@@ -170,6 +170,7 @@ monthly_max['ARR_DELAY'] = monthly_max['ARR_DELAY'].astype(int)
 monthly_max.rename(columns={'ARR_DELAY': 'Longest Delay (mins)'}, inplace=True)
 
 st.subheader("Longest Delay by Month and Airline")
+monthly_max = monthly_max.reset_index(drop=True)
 styled_table = monthly_max.style.set_properties(subset=['MONTH', 'Longest Delay (mins)'], **{'text-align': 'center'})
 st.write(styled_table.to_html(index=False), unsafe_allow_html=True)
 
