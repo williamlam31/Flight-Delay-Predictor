3b76d965b711b4ee44e7a0fae59575aba9f7c13d
williamlam31 <williamlam31@gmail.com>
2025-08-12 20:36:06 -0400

    Update flight_app.py

--- flight_app.py
+++ flight_app.py
@@ -63,7 +63,7 @@ def train_models():
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.30, random_state=42, stratify=y
-)
+    )
 
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
